057191d1482184acdd94ed0f12e2fd589537b597
Masoud Kazakh <kzmasut@gmail.com>
2025-05-22 12:33:47 +0330

    fix: .env.example not exists

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -6,6 +6,3 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS", "").split(",")
 DEBUG = config("DEBUG") == "True"
 SECRET_KEY = config("DJANGO_SECRET_KEY")
 STATIC_URL = config("STATIC_URL")
-STATIC_ROOT = config("STATIC_ROOT")
-MEDIA_URL = config("MEDIA_URL")
-MEDIA_ROOT = config("MEDIA_ROOT")
