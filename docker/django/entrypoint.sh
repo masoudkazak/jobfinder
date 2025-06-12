c66637894e3c939b6a4d29bd3f979098ce59a9a4
Masoud Kazakh <kzmasut@gmail.com>
2025-06-12 12:57:06 +0330

    feat:start scrape jobs

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -6,7 +6,7 @@ python manage.py wait_for_db
 python manage.py migrate --noinput
 python manage.py seed_provinces
 
-cat ./ecommerce.asci || echo "ascii file not found."
+cat ./robot.asci || echo "ascii file not found."
 
 if [ "$ENVIRONMENT" = "dev" ]; then
   python manage.py runserver 0.0.0.0:8000
